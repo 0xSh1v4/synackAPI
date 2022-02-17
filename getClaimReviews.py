8bf3d1c38330f5a06a13ee61af15c20a64e3615b
Siva Rajendran <sivarajendran@pm.me>
2022-02-17 17:27:56 +0100

    Function to view the pending claims

---
+++ getClaimReviews.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+from synack import synack
+import telegram
+import json
+
+def telegram_notify(message):
+    """AI is creating summary for telegram_notify
+
+    Args:
+        message ([type]): [description]
+    """
+    token = s1.telegramToken
+    chat_id = s1.telegramChatId
+    notify = telegram.Bot(token=token)
+    notify.sendMessage(chat_id=chat_id, text=message)
+
+
+s1 = synack()
+s1.gecko = False
+s1.getSessionToken()
+#s1.getAllTargets()
+response = s1.try_requests("GET", "https://platform.synack.com/api/tasks/v2/tasks?perPage=20&viewed=true&page=1&status=FOR_REVIEW&includeAssignedBySynackUser=true", 10)
+jsonResponse=response.json()
+if len(jsonResponse) > 0:
+    for i in range(len(jsonResponse)):
+        machine = jsonResponse[i]['title']
+        target = jsonResponse[i]['listingCodename']
+        amount = jsonResponse[i]['payout']['amount']
+        telegram_notify ("The machine name "+machine+" on the target "+target+" with the worth of ",amount," USD has been waiting for approval")
+else:
+    telegram_notify ("No Missions are present at the moment")
+
+
+
+
